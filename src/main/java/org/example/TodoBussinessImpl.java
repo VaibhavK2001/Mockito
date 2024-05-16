7b22b85cf96dbb2c64333ac11c982e79873f0ba5
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-05-16 20:24:04 +0530

    By Using Stub

---
+++ src/main/java/org/example/TodoBussinessImpl.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoBussinessImpl {
+
+    private  TodoService todoService;
+
+    public TodoBussinessImpl(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    public List<String> retrieveTodosReleatedtoSpring(String user){
+        List<String> filterTodos = new ArrayList<>();
+        List<String> todos = todoService.retrieveTodos(user);
+        for (String todo : todos) {
+            if (todo.contains("spring")) {
+                filterTodos.add(todo);
+            }
+        }
+        return filterTodos;
+    }
+}
